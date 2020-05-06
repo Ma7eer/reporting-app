a3507adf37f6095397aae24d188248371ba1f514
Ma7eer <maher.alkendi2@gmail.com>
2020-05-06 19:45:57 +0400

    added upload functionality

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 const app = express();
 
 // parse application/x-www-form-urlencoded
@@ -8,12 +9,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "./images");
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}.jpeg`);
+  },
+});
+
+const upload = multer({ storage: Storage });
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.post("/", upload.array("photo", 3), (req, res) => {
+  console.log("file", req.files);
+  console.log("body", req.body);
   res.json({ message: "works" });
 });
 
